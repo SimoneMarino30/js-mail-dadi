db763a474cef6da16a1cd5de96619ef8a622f066
Simone Marino <simo.marino@outlook.it>
2023-01-27 09:25:45 +0100

    Miglioramento commenti per logica (MAIL)

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -7,13 +7,15 @@
 
 // CREO UNA LISTA DI EMAIL
 // CHIEDO ALL' UTENTE IL SUO INDIRIZZO EMAIL
+// CREO UNA VARIABILE FALSA PER L'ACCESSO
 // CONFRONTO LA MAIL UTENTE CON QUELLE DELLA LISTA CON UN CICLO FOR ()
+// IN CASO DI QUESTE CONDIZIONI, ACCESSO E' VERO
 // SE LA MAIL UTENTE E' UGUALE A UNA DELLA LISTA
 // PUO' ACCEDERE
 // SE LA MAIL UTENTE NON E' UGUALE A UNA DELLA LISTA
 // NON PUO' ACCEDERE
 
-
+// const button = document.getElementById("myBtn");
 // CREO UNA LISTA DI EMAIL
 
 let lista = ["primamail@gmail.it", "secondamail@yahoo.it", "terzamail@yahoo.com", "quartamail@outlook.it"];
@@ -23,23 +25,25 @@ console.log(lista);
 let richiestaEmail = prompt('Qual è il tuo indirizzo e-mail?');
 // console.log(richiestaEmail);
 
-// const button = document.getElementById("myBtn");
-
+// CREO UNA VARIABILE FALSA PER L'ACCESSO
 let accesso = false;
 
 // CONFRONTO LA MAIL UTENTE CON QUELLE DELLA LISTA CON UN CICLO FOR ()
-
 for (i = 0; i <= lista.length; i++) {
+    // IN CASO DI QUESTE CONDIZIONI, ACCESSO E' VERO
     accesso = true;
     console.log(i)
 }
 
+// SE LA MAIL UTENTE E' UGUALE A UNA DELLA LISTA
 
-
+// SE LA MAIL UTENTE E' UGUALE A UNA DELLA LISTA
 if (accesso = true) {
-
+    // PUO' ACCEDERE
     alert("PUOI ACCEDERE");
+    // SE LA MAIL UTENTE NON E' UGUALE A UNA DELLA LISTA
 } else {
+    // NON PUO' ACCEDERE
     alert("NON PUOI ACCEDERE");
 }
 
